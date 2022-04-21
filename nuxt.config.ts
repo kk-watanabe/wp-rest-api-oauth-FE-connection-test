94d878ff50ced5f03eb46b309303d75c3d2224bb
zukki30 <kw4869ify@gmail.com>
2022-04-22 04:40:50 +0900

    feat: Add useFetch using rule

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  head: {
+    charset: "utf-8",
+    viewport: "width=device-width, initial-scale=1",
+  },
+  publicRuntimeConfig: {
+    WP_REST_API_BASE_URL: process.env.WP_REST_API_BASE_URL,
+    WP_USER_NAME: process.env.WP_USER_NAME,
+    WP_USER_PASSWORD: process.env.WP_USER_PASSWORD
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,32 @@
+<script setup lang="ts">
+import { Buffer } from 'buffer'
+
+const route = useRoute()
+const config = useRuntimeConfig();
+const data = `${config.WP_USER_NAME}:${config.WP_USER_PASSWORD}`;
+const option = {
+  headers: {
+    Authorization: `Basic ${Buffer.from(data).toString('base64')}`,
+  },
+};
+const { data: post } = await useFetch(`${config.WP_REST_API_BASE_URL}/wp/v2/blog` , option);
+const title = "WP REST API OAuth test";
+
+onMounted(() => {
+  console.log("Home onMounted");
+  console.log(post.value, option);
+});
+</script>
+
 <template>
+  <Html lang="ja">
+    <Head>
+      <Title>{{ title }}</Title>
+      <Meta name="description" :content="`This is ${title} page`" />
+    </Head>
+  </Html>
+
   <div>
     test
   </div>
 </template>
-
-<script setup lang="ts">
-const route = useRoute()
-
-onMounted(() => {
-  console.log("Home onMounted");
-  console.log(route);
-});
-</script>
